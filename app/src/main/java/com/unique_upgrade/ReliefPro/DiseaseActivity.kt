fb4901e18d2eb291f729643c60ee97d1c527accd
Hemanshu Parmar <parmarhemanshu733@gmail.com>
2021-05-13 16:49:08 +0530

    Final version

--- app/src/main/java/com/unique_upgrade/ReliefPro/DiseaseActivity.kt
+++ app/src/main/java/com/unique_upgrade/ReliefPro/DiseaseActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.unique_upgrade.ReliefPro.DiseaseActivity
+
 import com.unique_upgrade.ReliefPro.adapter.DiseaseAdapter
 import com.unique_upgrade.ReliefPro.model.DiseaseModel
 import java.util.*
@@ -29,44 +29,45 @@ class DiseaseActivity : AppCompatActivity() {
                 }
         )
         diseaseModelArrayList = ArrayList()
+
         diseaseModelArrayList.add(DiseaseModel("Corona Consulting",
                 "Headache,fever,throat pain...", R.drawable.coronaconsulting))
 
-        diseaseModelArrayList.add(DiseaseModel("General Physician", "Overall body problems..", R.drawable.general))
+        diseaseModelArrayList.add(DiseaseModel("Gynecology",
+                "Abnormal behaviour of sexual organs..", R.drawable.fullbodyicon))
 
 
-        diseaseModelArrayList.add(DiseaseModel("Pediatrician", "Children diseases and problems..", R.drawable.pediatrition))
+        diseaseModelArrayList.add(DiseaseModel("General Physician", "Overall body problems..",
+                R.drawable.general))
 
-        diseaseModelArrayList.add(DiseaseModel("Hair and Dermatology", "Hair and skin problems..", R.drawable.dermitology))
-
-
-        diseaseModelArrayList.add(DiseaseModel("Ayurveda", "Aayurvedic solution to all diseases", R.drawable.ayurveda))
-
-        diseaseModelArrayList.add(DiseaseModel("Cardiology", "Abnormality in heart beats..", R.drawable.cardiologyicon))
-
+        diseaseModelArrayList.add(DiseaseModel("Dermatology and Hair", "Hair and skin problems..",
+                R.drawable.dermitologyicon))
 
         diseaseModelArrayList.add(DiseaseModel("Sexology",
                 "Disease related to sexual organs..", R.drawable.sexology))
 
-        diseaseModelArrayList.add(DiseaseModel("ENT",
-                "Disease related to ear,nose and throat..", R.drawable.ent))
-
-        diseaseModelArrayList.add(DiseaseModel("Psychology",
-                "Mental problems, diseases..", R.drawable.physco))
-
-        diseaseModelArrayList.add(DiseaseModel("Gynecology",
-                "Abnormal behaviour of sexual organs..", R.drawable.gynacicon))
-
+        diseaseModelArrayList.add(DiseaseModel("Psychiatry",
+                "Mental problems, diseases..", R.drawable.physcoicon))
 
         diseaseModelArrayList.add(DiseaseModel("Stomach & Digestion",
                 "Stomach, Digestive problems..", R.drawable.stomach))
 
-        diseaseModelArrayList.add(DiseaseModel("Dental", "Tooth-ache, Pain in chewing food..", R.drawable.tooth))
+
+        diseaseModelArrayList.add(DiseaseModel("Pediatrician", "Children diseases and problems..",
+                R.drawable.pediatrition))
+
+        diseaseModelArrayList.add(DiseaseModel("ENT",
+                "Disease related to ear,nose and throat..", R.drawable.ent))
+
+        diseaseModelArrayList.add(DiseaseModel("Cardiology", "Abnormality in heart beats..",
+                R.drawable.cardiologyicon))
 
 
+        diseaseModelArrayList.add(DiseaseModel("Dental", "Tooth-ache, Pain in chewing food..",
+                R.drawable.tooth))
 
-
-
+        diseaseModelArrayList.add(DiseaseModel("Ayurveda", "Aayurvedic solution to all diseases",
+                R.drawable.ayurveda))
 
         val adapter = DiseaseAdapter(this, diseaseModelArrayList!!)
         val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
